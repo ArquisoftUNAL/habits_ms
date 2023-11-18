c72df2784e439fd948534449ae6167f3d8a4c7b4
edgardanielgd <edgardanielgd123@gmail.com>
2023-11-17 22:06:25 -0500

    Configured database replication, now lets see how to deploy this on GKE...

---
+++ database/master/replace-credentials.sh
@@ -0,0 +1,15 @@
+
+reader_user=$(echo $POSTGRESQL_READER_USER)
+reader_password=$(echo $POSTGRESQL_READER_PASSWORD)
+database=$(echo $POSTGRESQL_DATABASE)
+
+echo "Replacing credentials in create-reader-user.sql"
+echo "reader_user: $reader_user"
+echo "reader_password: $reader_password"
+echo "database: $database"
+
+sed -i "s#POSTGRESQL_READER_USER#$reader_user#g" /docker-entrypoint-initdb.d/create-reader-user.sql
+sed -i "s#POSTGRESQL_READER_PASSWORD#\'$reader_password\'#g" /docker-entrypoint-initdb.d/create-reader-user.sql
+sed -i "s#POSTGRESQL_DATABASE#$database#g" /docker-entrypoint-initdb.d/create-reader-user.sql
+
+cat /docker-entrypoint-initdb.d/create-reader-user.sql
